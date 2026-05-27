283caa2ca544cd59bda0dab7428af6b73c3a3c41
mwrobel7 <mariwrobel2007@gmail.com>
2026-05-27 19:27:25 +0200

    styles

--- nextjs-dashboard/app/app-settings/page.tsx
+++ nextjs-dashboard/app/app-settings/page.tsx
@@ -5,9 +5,13 @@ export default function AppSettingsPage() {
   return (
     <main className="min-h-screen flex">
       <Sidebar />
-      <h1 className="text-3xl font-bold">
-        Ustawienia aplikacji
-      </h1>
+      <section className="flex-1 p-10">
+        <div className="flex items-center justify-between mb-10">
+          <h1 className="text-3xl font-bold">
+            Ustawienia aplikacji
+          </h1>
+        </div>
+      </section>
     </main>
   )
 }
